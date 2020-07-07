fd69523453d6d6ddc5b30cdd66936c896c7da445
stoic-park <demige79@gmail.com>
2020-07-07 22:46:45 +0900

    :wrench: Fix: Chat.js socket fix
    - 소켓 채팅 클라이언트 부분 수정

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,12 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
+
+// 하위 컴포넌트
+import Messages from "../Messages/Messages";
 
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
   const ENDPOINT = "localhost:5000";
 
   useEffect(() => {
@@ -21,7 +27,7 @@ const Chat = ({ location }) => {
 
     setName(name);
     setRoom(room);
-    // console.log(name, room); // lama peru
+    console.log(name, room); // lama peru
 
     // console.log(socket);
     socket.emit("join", { name, room }, () => {
@@ -36,7 +42,41 @@ const Chat = ({ location }) => {
     };
   }, [ENDPOINT, location.search]); // 한번만 부른다 // 불필요한 사이드 이펙트를 줄인다
 
-  return <h1>Chat</h1>;
+  useEffect(() => {
+    // 서버에서 message 이벤트가 올 경우에 대해서 `on`
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  // 메세지 보내기 함수
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, setMessage(""));
+    }
+  };
+
+  console.log(message, messages);
+
+  // return <h1>Chat</h1>;
+  return (
+    <div className="outerContainer">
+      <div className=" container">
+        <div>{room}</div>
+        <a href="/">close</a>
+        <div></div>
+        <Messages messages={messages} name={name} />
+
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={(e) => (e.key === "Enter" ? sendMessage(e) : null)}
+        />
+        <button onClick={(e) => sendMessage(e)}>send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
