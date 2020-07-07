0b52aed03e75f0bfadd76f2b2c6df46594fac742
stoic-park <demige79@gmail.com>
2020-07-07 22:50:34 +0900

    :wrench: Fix: 필요 없는 파일 정리
    - 필요 없는 파일 정리

--- README.md
+++
@@ -1,9 +0,0 @@
-### lamaChat
-
-#### React + Socket.io + node.js 를 이용한 실시간 채팅 앱입니다.
-
-기존 react-chat 학습에서 추가 학습을 위한 프로젝트입니다
-
-1. socket.io 에서 1개 이상의 room을 다룬다
-2. 배포까지 진행한다
-3. material-ui를 적용
--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-function App() {
-  return <div className="App"></div>;
-}
-
-export default App;
