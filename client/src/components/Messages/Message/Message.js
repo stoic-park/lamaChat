9bed8c25c90ef0802daa59ed7d117a1aad470171
stoic-park <demige79@gmail.com>
2020-07-07 22:47:52 +0900

    :rocket: Feat: 클라이언트 메세지 컴포넌트 생성
    - Messages => Message

---
+++ client/src/components/Messages/Message/Message.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Message = ({ message: { text, user }, name }) => {
+  let isSentByCurrentUser = false;
+  const trimmedName = name.trim().toLowerCase();
+
+  if (user === trimmedName) {
+    isSentByCurrentUser = true;
+  }
+
+  return isSentByCurrentUser ? (
+    <div>
+      <p>{trimmedName}</p>
+      <p>{text}</p>
+    </div>
+  ) : (
+    <div>
+      <p>{text}</p>
+      <p>{user}</p>
+    </div>
+  );
+};
+
+export default Message;
---
+++ client/src/components/Messages/Messages.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Message from "./Message/Message";
+
+const Messages = ({ messages, name }) => (
+  <div className="messages">
+    {messages.map((message, i) => (
+      <div key={i}>
+        <Message message={message} name={name} />
+      </div>
+    ))}
+  </div>
+);
+
+export default Messages;
