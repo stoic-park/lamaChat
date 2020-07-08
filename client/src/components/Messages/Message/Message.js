234f317e9af1656eec587e86640bdc937f642796
stoic-park <demige79@gmail.com>
2020-07-08 17:06:13 +0900

    :wrench: Fix: 유저 구분 수정
    - 메세지 입력하는 유저가 다른 경우 처리

--- client/src/components/Messages/Message/Message.js
+++ client/src/components/Messages/Message/Message.js
@@ -3,11 +3,9 @@ import React from "react";
 const Message = ({ message: { text, user }, name }) => {
   let isSentByCurrentUser = false;
   const trimmedName = name.trim().toLowerCase();
-
   if (user === trimmedName) {
     isSentByCurrentUser = true;
   }
-
   return isSentByCurrentUser ? (
     <div>
       <p>{trimmedName}</p>
@@ -19,6 +17,12 @@ const Message = ({ message: { text, user }, name }) => {
       <p>{user}</p>
     </div>
   );
+  // return (
+  //   <div>
+  //     <p>{name}</p>
+  //     <p>{text}</p>
+  //   </div>
+  // );
 };
 
 export default Message;
