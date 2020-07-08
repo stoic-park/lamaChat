9e2edce7198d7e076f3474f7571f6b5f89ea8893
stoic-park <demige79@gmail.com>
2020-07-08 17:04:30 +0900

    :wrench: Fix: Chat.js 에러 처리
    - join 이벤트 생성시 응답으로 에러를 받을 경우 처리
    - 추가 소켓 함수 처리

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -4,6 +4,8 @@ import io from "socket.io-client";
 
 // 하위 컴포넌트
 import Messages from "../Messages/Messages";
+import RoomInfo from "../RoomInfo/RoomInfo";
+import Input from "../Input/Input";
 
 let socket;
 
@@ -12,6 +14,8 @@ const Chat = ({ location }) => {
   const [room, setRoom] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
+
+  const [users, setUsers] = useState("");
 
   const ENDPOINT = "localhost:5000";
 
@@ -27,25 +31,33 @@ const Chat = ({ location }) => {
 
     setName(name);
     setRoom(room);
+
     console.log(name, room); // lama peru
 
     // console.log(socket);
-    socket.emit("join", { name, room }, () => {
+    socket.emit("join", { name, room }, (error) => {
       // console.log("error");
-      // alert(error);
+      // 에러 처리
+      if (error) {
+        alert(error);
+      }
     });
 
-    return () => {
-      socket.emit("disconnect");
+    // return () => {
+    //   socket.emit("disconnect");
 
-      socket.off();
-    };
+    //   socket.off();
+    // };
   }, [ENDPOINT, location.search]); // 한번만 부른다 // 불필요한 사이드 이펙트를 줄인다
 
   useEffect(() => {
     // 서버에서 message 이벤트가 올 경우에 대해서 `on`
     socket.on("message", (message) => {
       setMessages([...messages, message]);
+    });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
     });
   }, [messages]);
 
@@ -58,22 +70,32 @@ const Chat = ({ location }) => {
   };
 
   console.log(message, messages);
+  console.log(users, "users");
 
   // return <h1>Chat</h1>;
+  // 1.roominfo
+  // 2.messages
+  // 3.input
   return (
     <div className="outerContainer">
-      <div className=" container">
-        <div>{room}</div>
-        <a href="/">close</a>
-        <div></div>
+      <div className="container">
+        {/* <div className="roominfo">
+          <div>{room}</div>
+          <a href="/">close</a>
+        </div> */}
+        <RoomInfo room={room} />
         <Messages messages={messages} name={name} />
-
-        <input
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+        {/* <input
           value={message}
           onChange={(e) => setMessage(e.target.value)}
           onKeyPress={(e) => (e.key === "Enter" ? sendMessage(e) : null)}
         />
-        <button onClick={(e) => sendMessage(e)}>send</button>
+        <button onClick={(e) => sendMessage(e)}>send</button> */}
       </div>
     </div>
   );
