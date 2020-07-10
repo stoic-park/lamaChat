898ac625c6cc99d7a9f23a5b04c41a17cf04eb0e
stoic-park <demige79@gmail.com>
2020-07-10 15:03:31 +0900

    :wrench: Fix: JoinRoom CSS Grid
    - Material UI를 이용해서 레이아웃 수정

--- client/src/components/JoinRoom/JoinRoom.js
+++ client/src/components/JoinRoom/JoinRoom.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 // Material-UI 적용하기
 import Avatar from "@material-ui/core/Avatar";
@@ -8,7 +8,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
+// import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
@@ -21,7 +21,10 @@ const useStyles = makeStyles((theme) => ({
     height: "100vh",
   },
   image: {
-    backgroundImage: "url(https://source.unsplash.com/random)",
+    // backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundImage: "url(https://source.unsplash.com/1600x900/?lama,llama)",
+    // backgroundImage:
+    //   "url(https://images.unsplash.com/photo-1518259102261-b40117eabbc9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80)",
     backgroundRepeat: "no-repeat",
     backgroundColor:
       theme.palette.type === "light"
@@ -45,6 +48,7 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(1),
   },
   submit: {
+    // width: "100%",
     margin: theme.spacing(3, 0, 2),
   },
 }));
@@ -55,13 +59,37 @@ const JoinRoom = () => {
   const classes = useStyles();
 
   return (
-    <div className="joinOuterContainer">
-      <div className="joinInnerContainer">
-        {/* <h1 className="head">JoinRoom</h1> */}
-        <Typography component="h1" variant="h5">
-          Join LamaChat
-        </Typography>
-        {/* <div>
+    <Grid container component="main" className={classes.root}>
+      {/* <div className="joinOuterContainer"> */}
+      {/* <CssBaseline /> */}
+      <Grid container spacing={5}>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={7}
+          lg={7}
+          xl={7}
+          className={classes.image}
+        />
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={5}
+          lg={5}
+          xl={5}
+          component={Paper}
+          elevation={6} // 그림자?
+          square
+        >
+          <div className={classes.paper}>
+            {/* <div className="joinInnerContainer"> */}
+            {/* <h1 className="head">JoinRoom</h1> */}
+            <Typography component="h1" variant="h5">
+              Join LamaChat
+            </Typography>
+            {/* <div>
           <input
             placeholder="Name"
             className="joinInput"
@@ -69,34 +97,34 @@ const JoinRoom = () => {
             onChange={(e) => setName(e.target.value)}
           />
         </div> */}
-        {/* <form className={classes.form} noValidate> */}
-        <form className="joinInput" noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="name"
-            label="Name"
-            name="name"
-            autoComplete="name"
-            autoFocus
-            onChange={(e) => setName(e.target.value)}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="room"
-            label="room"
-            type="room"
-            id="room"
-            autoComplete="current-password"
-            onChange={(e) => setRoom(e.target.value)}
-          />
-        </form>
-        {/* <div>
+            {/* <form className={classes.form} noValidate> */}
+            <form className={classes.form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Name"
+                name="name"
+                autoComplete="name"
+                autoFocus
+                onChange={(e) => setName(e.target.value)}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="room"
+                label="room"
+                type="room"
+                id="room"
+                autoComplete="current-password"
+                onChange={(e) => setRoom(e.target.value)}
+              />
+
+              {/* <div>
           <input
             placeholder="Room"
             className="joinInput mt-20" // margin top 20
@@ -104,27 +132,33 @@ const JoinRoom = () => {
             onChange={(e) => setRoom(e.target.value)}
           />
         </div> */}
-        <Link
-          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
-          // to={`/chat?name=${name}&room=${room}`}
-          href={`/chat?name=${name}&room=${room}`}
-        >
-          {/* <button className="button mt-20" type="submit">
+
+              <Link
+                onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+                to={`/chat?name=${name}&room=${room}`}
+                // href={`/chat?name=${name}&room=${room}`}
+              >
+                {/* <button className="button mt-20" type="submit">
             Sign In
           </button> */}
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            // className={classes.submit}
-            className="button"
-          >
-            Sign In
-          </Button>
-        </Link>
-      </div>
-    </div>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                  // className="button"
+                >
+                  Sign In
+                </Button>
+              </Link>
+            </form>
+            {/* </div> */}
+          </div>
+        </Grid>
+      </Grid>
+      {/* </div> */}
+    </Grid>
   );
 };
 
