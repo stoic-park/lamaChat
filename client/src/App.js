f2a00ce0d7b9e711f66d5271a2db9b673e51a45b
stoic-park <demige79@gmail.com>
2020-07-10 16:46:36 +0900

    :wrench: Fix: App.js css fix
    - 마진값 수정했습니다

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import JoinRoom from "./components/JoinRoom/JoinRoom";
