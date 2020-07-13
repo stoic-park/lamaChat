3030940a339a53f565282b29edee0dfa7f12ed27
stoic-park <demige79@gmail.com>
2020-07-13 20:58:41 +0900

    Updates

---
+++ precache-manifest.09585affc36e90dd645eeecb1fc1beb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3036a38cf814fd10703a53a10afa293",
+    "url": "/lamaChat/index.html"
+  },
+  {
+    "revision": "98a3e32c361be67b8e5f",
+    "url": "/lamaChat/static/css/main.4963c6f4.chunk.css"
+  },
+  {
+    "revision": "6479a406c30380919b18",
+    "url": "/lamaChat/static/js/2.2d6b29b9.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/lamaChat/static/js/2.2d6b29b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a3e32c361be67b8e5f",
+    "url": "/lamaChat/static/js/main.990ab31a.chunk.js"
+  },
+  {
+    "revision": "b4191a38961632702606",
+    "url": "/lamaChat/static/js/runtime-main.0d805478.js"
+  }
+]);
